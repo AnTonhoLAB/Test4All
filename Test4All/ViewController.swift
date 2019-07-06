d7c1b7fc73aa587e13434a34f7d8b758105921a7
George Gomes <georgegomees@gmail.com>
2019-07-06 17:30:11 -0300

    Setup project

---
+++ Test4All/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test4All
+//
+//  Created by George Gomes on 06/07/19.
+//  Copyright © 2019 George Gomes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
